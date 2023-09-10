807124f39006a2c6b977b5501a675ba4b74f80fa
unknown <jibon602947@gmail.com>
2023-09-10 21:40:31 +0600

    logout

--- frontend/src/app/layouts/full/header/header.component.ts
+++ frontend/src/app/layouts/full/header/header.component.ts
@@ -5,12 +5,18 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class AppHeaderComponent {
   role: any;
-  constructor(private router: Router,
-    private dialog: MatDialog) {
+  constructor(private router: Router, private dialog: MatDialog) {}
 
+  logout() {
+    let confrimation = 'Want to Logout?';
+    if (confirm(confrimation) == true) {
+      localStorage.clear();
+      this.router.navigate(['/']);
+    } else {
+    }
   }
 }
