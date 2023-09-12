fb23a459001e82ba2d2b7a57eb4825684084d09b
unknown <jibon602947@gmail.com>
2023-09-12 17:04:44 +0600

    view bill

--- frontend/src/app/material-component/dialog/view-bill-products/view-bill-products.component.ts
+++ frontend/src/app/material-component/dialog/view-bill-products/view-bill-products.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-view-bill-products',
   templateUrl: './view-bill-products.component.html',
-  styleUrls: ['./view-bill-products.component.scss']
+  styleUrls: ['./view-bill-products.component.scss'],
 })
 export class ViewBillProductsComponent implements OnInit {
+  displayedColumns: string[] = [
+    'name',
+    'category',
+    'quantity',
+    'price',
+    'total',
+  ];
+  dataSource: any = [];
+  data: any;
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public dialogData: any,
+    public dialogRef: MatDialogRef<ViewBillProductsComponent>
+  ) {}
 
-  
   ngOnInit() {
+    this.data = this.dialogData.data;
+    // console.log(this.data.productDetails);
+    this.dataSource = JSON.parse(this.data.productDetails);
   }
 }
